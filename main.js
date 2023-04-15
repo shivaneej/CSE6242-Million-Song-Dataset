4cc8e759dadf19cf89749147852409d3ecb041b5
shivaneej <shivaneej02@gmail.com>
2023-04-14 23:16:43 -0400

    Major revision: use force simulation and better expansion and collapse

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ var margins = { left: 50, right: 50, top: 50, bottom: 50 };
 var networkGraphWidth = width - margins.left - margins.right;
 var networkGraphHeight = height - margins.top - margins.bottom;
 var radiusScale = d3.scaleLinear().range([5, 25]);
-const colors = {'SELECTED': '#E0538F', 'DEFAULT' : '#2E64A2' };
+const colors = { 'SELECTED': '#E0538F', 'DEFAULT': '#2E64A2' };
+var nodes, edges, allNodesMap, artistEdges;
+var sliderValue;
+var graphData, graph, selectedArtist, graphDataMap;
+var force;
 
 const slider = document.getElementById("similar_count_slider");
 
@@ -17,112 +21,122 @@ let tip = d3.tip().attr('class', 'd3-tip').attr("id", "tooltip");
 const search = document.getElementById("search");
 
 Promise.all([
-    d3.dsv(",", edgesFilePath, function(edge) {
+    d3.dsv(",", edgesFilePath, function (edge) {
         return {
-            source_artist_id: edge.source_artist_id,
-            target_artist_id: edge.target_artist_id,
+            source: edge.source_artist_id,
+            target: edge.target_artist_id,
             priority: parseInt(edge.priority)
         };
     }),
     d3.dsv(",", nodesFilePath, (node) => {
         return {
-            artist_id: node.artist_id, 
-            artist_name: node.artist_name, 
-            avg_duration: parseFloat(node.avg_duration), 
-            avg_familiarity: parseFloat(node.avg_familiarity), 
-            avg_hotness: parseFloat(node.avg_hotttnesss), 
+            artist_id: node.artist_id,
+            artist_name: node.artist_name,
+            avg_duration: parseFloat(node.avg_duration),
+            avg_familiarity: parseFloat(node.avg_familiarity),
+            avg_hotness: parseFloat(node.avg_hotttnesss),
             total_tracks: parseInt(node.total_tracks)
         };
     })
 ]).then(allData => {
-    let edges = allData[0]; // all edges data from csv file
-    let nodes = allData[1]; // all node data from the csv file
-
-    let allNodesMap = nodes.reduce((obj, item) => { 
+    edges = allData[0]; // all edges data from csv file
+    nodes = allData[1]; // all node data from the csv file
+    let minTracks = maxTracks = nodes[0]['total_tracks'];
+    allNodesMap = nodes.reduce((obj, item, idx) => {
+        item['index'] = idx;
+        item.children = null;
         obj[item['artist_id']] = item;
+        minTracks = Math.min(minTracks, item['total_tracks']);
+        maxTracks = Math.max(maxTracks, item['total_tracks']);
         return obj;
     }, {}); // map for quick lookup of nodes by id
 
-    var svg = d3.select("body").append("svg")
-                .attr("width", width)
-                .attr("height", height);
+    radiusScale.domain([minTracks, maxTracks]);
 
-    var graph = svg.append("g")
-                .attr("width", width - margins.left - margins.right)
-                .attr("height", height - margins.top - margins.bottom)
-                .attr("transform", "translate( " + margins.left + ", "+ margins.top + ")");
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    graph = svg.append("g")
+        .attr("width", networkGraphWidth)
+        .attr("height", networkGraphHeight)
+        .attr("transform", "translate( " + margins.left + ", " + margins.top + ")");
+
 
     // Show initial network of artist based on selected artist (How many neighbors to show in the beginning?)
-    var selectedArtist = nodes[0];
-    var sliderValue = 5;
-    drawNetworkGraph(graph, edges, allNodesMap, selectedArtist, sliderValue);
+    selectedArtist = nodes[0];
+    sliderValue = 5;
 
+    fetchGraphData(selectedArtist);
+    graphDataMap = buildGraphDataMap({});
+    drawGraph();
 
     // List of artists to display
-    
     var selectTag = d3.select("select");
 
     var options = selectTag.selectAll('option')
-      .data(nodes.slice(0,10000));
+        .data(nodes.slice(0, 10000));
 
     options.enter()
-      .append('option')
-      .attr('value', function(d) {
-        return d.artist_name;
-      })
-      .attr('id',function(d){
-        return d.artist_id;
-      })
-      .text(function(d) {
-        return d.artist_name
-      });
+        .append('option')
+        .attr('value', function (d) {
+            return d.artist_name;
+        })
+        .attr('id', function (d) {
+            return d.artist_id;
+        })
+        .text(function (d) {
+            return d.artist_name
+        });
 
-      search.addEventListener("click", function(){
+    search.addEventListener("click", function () {
         var e = document.getElementById("artists")
         var text = e.options[e.selectedIndex]
         selectedArtist = allNodesMap[text.id]
 
-        clearGraph(graph);
-        drawNetworkGraph(graph, edges, allNodesMap, selectedArtist, sliderValue);
+        clearGraph();
+        fetchGraphData(selectedArtist);
+        graphDataMap = buildGraphDataMap({});
+        drawGraph();
+    })
 
-      })
-    
     // Display initial nodes of top artists to select from
-    
-    var topDiv = d3.select("#top_artists");
-    var topArtistList = nodes.sort((a,b) => b.avg_familiarity - a.avg_familiarity); 
-    
-    var disc = topDiv
-                .selectAll(".disc")
-                .data(topArtistList.slice(0,9))
-                .enter()
-                .append("button")
-                .style("padding","5px")
-                .style("margin","5px")
-                .attr("id",(d)=> d.artist_id)
-                .attr("class","disc")
-                .on("click",function(d) {
-                    selectedArtist = allNodesMap[d.artist_id]
 
-                    clearGraph(graph);
-                    drawNetworkGraph(graph, edges, allNodesMap, selectedArtist, sliderValue)
-                  });
+    var topDiv = d3.select("#top_artists");
+    var topArtistList = nodes.sort((a, b) => b.avg_familiarity - a.avg_familiarity);
+
+    var disc = topDiv
+        .selectAll(".disc")
+        .data(topArtistList.slice(0, 9))
+        .enter()
+        .append("button")
+        .style("padding", "5px")
+        .style("margin", "5px")
+        .attr("id", (d) => d.artist_id)
+        .attr("class", "disc")
+        .on("click", function (d) {
+            selectedArtist = allNodesMap[d.artist_id]
+            clearGraph();
+            fetchGraphData(selectedArtist);
+            graphDataMap = buildGraphDataMap({});
+            drawGraph();
+        });
 
     disc.append("text")
         .attr("stroke", "black")
         .attr("font-size", "11px")
         .attr("text-anchor", "middle")
-        .text(function(d) {
+        .text(function (d) {
             return d['artist_name'];
-    });
-
-
+        });
 
     //   Slider 
-    slider.addEventListener("input", function() {
+    slider.addEventListener("input", function () {
         sliderValue = this.value
-        clearGraph(graph);
-        drawNetworkGraph(graph, edges, allNodesMap, selectedArtist, sliderValue);
+        clearGraph();
+        fetchGraphData(selectedArtist);
+        graphDataMap = buildGraphDataMap({});
+        drawGraph();
     });
 
 
@@ -133,7 +147,7 @@ Promise.all([
     // Any other styling for selected node
 
     // tooltip for nodes
-    tip.html(function(d) { 
+    tip.html(function (d) {
         return getTooltipStats(d);
     });
     graph.call(tip);
@@ -143,102 +157,182 @@ Promise.all([
     console.log(error)
 });
 
+/**
+ * Build a map of all current nodes in the network
+ * The id of the nodes are the keys in the map
+ * The node objects are the values
+ * @param currentMap existing map to add the nodes to
+ */
+function buildGraphDataMap(currentMap) {
+    graphData.forEach(node => {
+        currentMap[node['artist_id']] = node;
+    });
+    return currentMap;
+}
 
+
+/**
+ * Function to get nodes and edges in the form required for force simulation
+ * @param {*} selectedArtist node that was selected
+ */
+function fetchGraphData(selectedArtist) {
+    selectedArtist.children = [];
+    graphData = [selectedArtist];
+    artistEdges = getArtistNetwork(selectedArtist['artist_id'], sliderValue);
+    artistEdges.forEach(edge => {
+        var target = allNodesMap[edge['target']];
+        graphData.push(target);
+        selectedArtist.children.push(target);
+    });
+}
+
+/**
+ * Function to get the data to show in the tooltip
+ * @param {*} hoveredNode node which is currently hovered
+ * @returns 
+ */
 function getTooltipStats(hoveredNode) {
-    return "Artist Name: " + hoveredNode['artist_name'] + 
-    "<br> Average Duration: " + parseFloat(hoveredNode['avg_duration']).toFixed(2) + 
-    "<br> Average Hotness: " + parseFloat(hoveredNode['avg_hotness']).toFixed(2) + 
-    "<br> Average Familiarity: " + parseFloat(hoveredNode['avg_familiarity']).toFixed(2) + 
-    "<br> Total Tracks: " + hoveredNode['total_tracks'] ;
+    return "Artist Name: " + hoveredNode['artist_name'] +
+        "<br> Average Duration: " + parseFloat(hoveredNode['avg_duration']).toFixed(2) +
+        "<br> Average Hotness: " + parseFloat(hoveredNode['avg_hotness']).toFixed(2) +
+        "<br> Average Familiarity: " + parseFloat(hoveredNode['avg_familiarity']).toFixed(2) +
+        "<br> Total Tracks: " + hoveredNode['total_tracks'];
 }
 
 /**
  * To get the similar artist network from list of edges
- * @param edges: array of all edges
  * @param artist_id: id of the artist to find the network for
  * @param count: number of similar artists to return sorted by priority
  */
-function getArtistNetwork(edges, artist_id, count = 20) {
-    return edges.filter(edge => edge['source_artist_id'] === artist_id)
-            .sort((edge1, edge2) => edge1['priority'] - edge2['priority'])
-            .slice(0, count);
+function getArtistNetwork(artist_id, count = 20) {
+    let filtered = edges.filter(edge => edge['source'] === artist_id);
+
+    //create a deep copy of the edges because forceSimulation modifies these edges
+    let neighbors = JSON.parse(JSON.stringify(filtered))
+    .sort((edge1, edge2) => edge1['priority'] - edge2['priority'])
+    .slice(0, count);
+    return neighbors;
 }
+
+/**
+ * Handle the tick event for force simulation
+ */
+function tick() {
+    path.attr("d", function (d) {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+        var test = "M" +
+            d.source.x + "," +
+            d.source.y + "A" +
+            dr + "," + dr + " 0 0,1 " +
+            d.target.x + "," +
+            d.target.y;
+        return test;
+    });
+
+    node.attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    });
+}
+
 
 /**
  * Clear the network by removing all children elements
  * @param graph group node under SVG
  */
-function clearGraph(graph) {
+function clearGraph() {
     graph.selectAll("*").remove();
 }
 
 /**
- * Create the network graph based on selected artist
- * @param {*} graph group node under SVG
- * @param {*} edges all edge information read from CSV file
- * @param {*} allNodesMap map containing information of all nodes read from CSV file
- * @param {*} selectedArtist selected artist
- * @param {*} numberNeighbors number of similar artists to show
+ * Function to plot the nodes, add force simulation, path, etc
  */
-function drawNetworkGraph(graph, edges, allNodesMap, selectedArtist, numberNeighbors = 5) {
-    let minTracks = maxTracks = selectedArtist['total_tracks'];
-    var artistNetwork = getArtistNetwork(edges, selectedArtist['artist_id'], numberNeighbors); 
-    console.log('selected artist is: ', selectedArtist);
-    console.log('similar artists are: ', artistNetwork);
+function drawGraph() {
+    if (force != null)
+        force.stop();
+    force = d3.forceSimulation()
+        .nodes(d3.values(graphDataMap))
+        .force("link", d3.forceLink(artistEdges).id(d => d['artist_id']).distance(150).strength(0.1))
+        .force('center', d3.forceCenter(networkGraphWidth / 2, networkGraphHeight / 2))
+        .force("x", d3.forceX())
+        .force("y", d3.forceY())
+        .force("charge", d3.forceManyBody().strength(-700))
+        .alphaTarget(0.1)
+        .on("tick", tick);
 
-    selectedArtist.x = Math.random() * networkGraphWidth; 
-    selectedArtist.y = Math.random() * networkGraphHeight; 
-    let nodesToDisplay = [selectedArtist];
-    artistNetwork.forEach(neighbor => {
-        let newNode = allNodesMap[neighbor['target_artist_id']];
-        newNode.x = Math.random() * networkGraphWidth; 
-        newNode.y = Math.random() * networkGraphHeight; 
-        minTracks = Math.min(minTracks, newNode['total_tracks']);
-        maxTracks = Math.max(maxTracks, newNode['total_tracks']);
-        nodesToDisplay.push(newNode);
-    });
-    console.log('all nodes in network: ', nodesToDisplay);
-    radiusScale.domain([minTracks, maxTracks]);
+    path = graph.append("g")
+        .selectAll("path")
+        .data(artistEdges)
+        .enter()
+        .append("path")
 
-
-    var node = graph
-                .selectAll(".node")
-                .data(nodesToDisplay)
-                .enter()
-                .append("g")
-                .attr("class", "node");
-
-    //Dynamic node size based on number of songs
-    node.append("circle")
-        .attr("cx", (d) => d.x)
-        .attr("cy", (d) => d.y)
-        .attr("r", (d) => radiusScale(d['total_tracks']))
-        .attr("fill", (d) => {
-            return (d['artist_id'] == selectedArtist['artist_id']) ? colors.SELECTED : colors.DEFAULT; 
-        })
+    node = graph.selectAll(".node")
+        .data(force.nodes())
+        .enter().append("g")
+        .attr("class", "node")
+        .on("dblclick", update)
         .on('mouseover', tip.show)
         .on('mouseout', tip.hide);
 
+    node.append("circle")
+        .attr("id", function (d) {
+            return d.id;
+        })
+        .attr("r", function (d) {
+            return radiusScale(d['total_tracks']);
+        })
+        .attr("fill", (d) => {
+            return (d['children'] != null) ? colors.SELECTED : colors.DEFAULT;
+        })
+
+
     node.append("text")
         .attr("stroke", "black")
-        .attr("font-size", "15px")
-        .attr("x", (d) => d.x )
-        .attr("y", (d) => d.y - 30 )
-        .attr("text-anchor", "middle")
-        .text(function(d) {
-            return d['artist_name'];
-    });
+        .attr("font-size", "12px")
+        .attr("x", 10)
+        .attr("y", -5)
+        .text(function (d) {
+            return (d.artist_name);
+        });
 
-    var path = graph
-                .selectAll("path")
-                .data(artistNetwork)
-                .enter()
-                .append("path");
+    force.alpha(0.1).restart()
+}
 
-    path.attr("d", function(d) {
-        var source = allNodesMap[d['source_artist_id']];
-        var target = allNodesMap[d['target_artist_id']];
-        var line =  "M" + source.x + "," + source.y + "L" + target.x + "," + target.y;
-        return line;
-    });
+/**
+ * Function to handle double click event of a node
+ * @param d node that was clicked
+ */
+function update(d) {
+    if (d.children != null) {
+        let childrenToDelete = d.children.map(child => child['artist_id']);
+        artistEdges = artistEdges.filter(edge => {
+            return !(edge['source']['artist_id'] == d['artist_id'] && childrenToDelete.includes(edge['target']['artist_id']))
+        });
+        var edgeTargets = artistEdges.map(edge => edge['target']['artist_id']);
+        graphData = graphData.filter(node => {
+            let key = node['artist_id'];
+            return edgeTargets.includes(key) || key == selectedArtist['artist_id']
+        });
+        graphDataMap = buildGraphDataMap({});
+        d.children = null;
+        clearGraph();
+        drawGraph();
+    }
+    else {
+        // get data of similar artists
+        let newArtistEdges = getArtistNetwork(d['artist_id'], sliderValue);
+        d.children = [];
+        newArtistEdges.forEach(edge => {
+            var target = allNodesMap[edge['target']];
+            if (graphData.filter(node => node['artist_id'] === target['artist_id']).length == 0) {
+                graphData.push(target);
+            }
+            d.children.push(target);
+        });
+        artistEdges = artistEdges.concat(newArtistEdges);
+        graphDataMap = buildGraphDataMap(graphDataMap);
+        clearGraph();
+        drawGraph();
+    }
 }
